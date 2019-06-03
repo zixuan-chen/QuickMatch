12563cf7413035569e511a20793f73fa3c272674
AC Violet <30395886+m13953842591@users.noreply.github.com>
2019-06-03 11:48:05 +0800

    update

--- test.py
+++ test.py
@@ -12,8 +12,50 @@ def get_size(path):
     return size
 
 
+def get_ground_truth(m, n):
+    """
+
+    :param m: 6
+    :param n: 13
+    :return:
+    """
+    ground_truth = []
+    for i in range(n):
+        ground_truth.append([j*n+i for j in range(m)])
+    return ground_truth
+
+
+def eval(pred, ground_truth):
+    num = len(ground_truth)
+    correct = 0
+    for p in pred:
+        if sorted(p) in ground_truth:
+            correct += 1
+    return correct / num
+
+
+def visualize():
+    for i in range(6):
+        cv2.namedWindow("imgs[%d]" % (i + 1), cv2.WINDOW_NORMAL)
+    imgs = np.zeros((6, col, row, 3), dtype=np.uint8)
+    for c in clusters:
+        for i in range(6):
+            img_path = data_path + "\\%s\\img%d.ppm" % (categroy, i + 1)
+            imgs[i] = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+        for idx in c:
+            i = idx // n
+            x, y = kp[idx][0], kp[idx][1]
+            cv2.circle(imgs[i], (x, y), 2, (0, 255, 255), 3)
+
+        for i in range(6):
+            cv2.imshow("imgs[%d]" % (i + 1), imgs[i])
+        cv2.waitKey(0)
+
+
+print(get_ground_truth(6, 13))
 data_path = "C:\\Users\\ChenZixuan\\Documents\\Graffiti_dataset"
-categroy = 'graf'
+categroy = 'wall'
 size = get_size(data_path + "\\size")
 row, col = size[categroy]
 
@@ -28,28 +70,7 @@ kp = kp.transpose(1, 0, 2).reshape(m*n, t)
 
 clusters = quickmatch_cluster(data, n, kernel=np.exp, flag_low_memory=False)
 
-for i in range(6):
-    cv2.namedWindow("imgs[%d]" % (i+1), cv2.WINDOW_NORMAL)
+ground_truth = get_ground_truth(m, n)
+correct_rate = eval(clusters, ground_truth)
+print("ground_truth_num=%d, accuracy:%.4f" % (n, correct_rate))
 
-# for i in range(kp.shape[0]):
-#
-#     for j in range(6):
-#         cv2.circle(imgs[j], (kp[i][j][0], kp[i][j][1]), 2, (0,255,255),3)
-#         cv2.imshow("imgs[%d]"%(j+1), imgs[j])
-#
-#     cv2.waitKey(0)
-imgs = np.zeros((6, col, row, 3), dtype=np.uint8)
-
-for c in clusters:
-    for i in range(6):
-        img_path = data_path + "\\%s\\img%d.jpg" % (categroy, i+1)
-        imgs[i] = cv2.imread(img_path, cv2.IMREAD_COLOR)
-
-    for idx in c:
-        i = idx // n
-        x, y = kp[idx][0], kp[idx][1]
-        cv2.circle(imgs[i], (x, y), 2, (0, 255, 255), 3)
-
-    for i in range(6):
-        cv2.imshow("imgs[%d]" % (i + 1), imgs[i])
-    cv2.waitKey(0)
